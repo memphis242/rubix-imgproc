2dc3c5e43cf7cfe2dfca7e643639e39c4d949aff
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-07-27 06:37:42 -0500

    :brain: `cvtColor` convert img to grayscale and `imshow`

--- src/rubix-imgproc.cpp
+++ src/rubix-imgproc.cpp
@@ -3,16 +3,26 @@
 
 int main(int argc, char * argv[])
 {
-   cv::Mat img = cv::imread("mixed-rubiks-cube.jpg");
-
+   // Read in our image and make sure it loaded
+   cv::Mat img = cv::imread("lrg-mixed-rubiks-cube.jpg");
    if ( img.empty() )
    {
       std::cerr << "Could not load image." << std::endl;
       return 1;
    }
 
+   /**************************** Process the Image ****************************/
+   // By default, cv::imread returns a BGR color-coded cv::Mat object
+   cv::Mat img_gray;
+   cv::cvtColor( img, img_gray, cv::COLOR_BGR2GRAY );
+
+   /***************************************************************************/
+
+   // Display the image(s)
    cv::namedWindow("Mixed Rubik's Cube", cv::WINDOW_AUTOSIZE);
    cv::imshow("Mixed Rubik's Cube", img);
+   cv::namedWindow("Mixed Rubik's Cube (Grayscale)", cv::WINDOW_AUTOSIZE);
+   cv::imshow("Mixed Rubik's Cube (Grayscale)", img_gray);
 
    // Wait indefinitely until a key is pressed
    cv::waitKey(0);
