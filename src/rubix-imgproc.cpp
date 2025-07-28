5ddb5313e1122bff72120721dfb5ee07b6fba8ae
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-07-27 23:08:09 -0500

    :brain: example Sobel edge detector

--- src/rubix-imgproc.cpp
+++ src/rubix-imgproc.cpp
@@ -43,15 +43,33 @@ int main(void)
    cv::Mat img_gaussianblur_manualk;
    cv::filter2D(img, img_gaussianblur_manualk, -1, k2d);
 
+   // Let's now try some edge detectors, starting with Sobel
+   // We've already got the grayscaled image, which is a typical first-step.
+   // Now, we'll blur it, and then apply the 1D gradient operations, then
+   // get the magnitude.
+   cv::Mat img_gray_blurred;
+   cv::GaussianBlur(img_gray, img_gray_blurred, cv::Size(3,3), 1.0);
+   cv::Mat grad_x, grad_y;
+   cv::Sobel(img_gray_blurred, grad_x, CV_32F, 1, 0, 3);
+   cv::Sobel(img_gray_blurred, grad_y, CV_32F, 0, 1, 3);
+   cv::Mat mag_grad;
+   cv::magnitude(grad_x, grad_y, mag_grad);
+   // Now that we've got the magnitude of the gradient, let's normalize before
+   // we display the image.
+   cv::Mat img_edges_sobel;
+   cv::normalize(mag_grad, img_edges_sobel, 0, 255, cv::NORM_MINMAX);
+   img_edges_sobel.convertTo(img_edges_sobel, CV_8U);
+
    /***************************************************************************/
 
    // Display the image(s)
    cv::imshow("Mixed Rubik's Cube", img);
-   cv::imshow("Mixed Rubik's Cube (Grayscale)",         img_gray);
-   cv::imshow("Filtered Image (Vertical Kernel)",       img_filtered_vert);
-   cv::imshow("Filtered Image (Horizontal Kernel)",     img_filtered_horz);
-   cv::imshow("Gaussian Blurred Image (Built-In)",      img_gaussianblur_builtin);
-   cv::imshow("Gaussian Blurred Image (Manual Kernel)", img_gaussianblur_manualk);
+   //cv::imshow("Mixed Rubik's Cube (Grayscale)",         img_gray);
+   //cv::imshow("Filtered Image (Vertical Kernel)",       img_filtered_vert);
+   //cv::imshow("Filtered Image (Horizontal Kernel)",     img_filtered_horz);
+   //cv::imshow("Gaussian Blurred Image (Built-In)",      img_gaussianblur_builtin);
+   //cv::imshow("Gaussian Blurred Image (Manual Kernel)", img_gaussianblur_manualk);
+   cv::imshow("Sobel'd Image",                          img_edges_sobel);
 
    // Wait indefinitely until a key is pressed
    cv::waitKey(0);
