bcd95e74d68056d07ab4482d77ca6d8fe72b71b9
Abdullah Almosalami <abdullah.almosalami.eng@gmail.com>
2025-07-31 17:50:10 -0500

    :pick: attempt downscale → edge filter →upscale

--- src/rubix-imgproc.cpp
+++ src/rubix-imgproc.cpp
@@ -7,70 +7,42 @@
 int main(void)
 {
    // Read in our image and make sure it loaded
-   cv::Mat img = cv::imread("mixed-rubiks-cube.jpg");
-   //cv::Mat img = cv::imread("lrg-mixed-rubiks-cube.jpg");
+   //cv::Mat img = cv::imread("mixed-rubiks-cube.jpg");
+   cv::Mat img = cv::imread("lrg-mixed-rubiks-cube.jpg");
    if ( img.empty() )
    {
       std::cerr << "Could not load image." << std::endl;
       return 1;
    }
+   cv::Size og_size = img.size();
 
-   /**************************** Process the Image ****************************/
-   // By default, cv::imread returns a BGR color-coded cv::Mat object
+   /**************************** Contour Detection ****************************/
+   // By default, cv::imread returns a BGR color-coded cv::Mat object, so
+   // convert to gray-scale.
    cv::Mat img_gray;
-   cv::cvtColor( img, img_gray, cv::COLOR_BGR2GRAY );
+   cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
 
-   //// Let's play with different kernels to convolve /w the image
-   //cv::Mat k_vert = (cv::Mat_<float>(3,3) <<
-   //      0, 1, 0,
-   //      0, 1, 0,
-   //      0, 1, 0 );
-   //cv::Mat img_filtered_vert;
-   //cv::filter2D(img_gray, img_filtered_vert, -1, k_vert);
+   /* Pre-process the Image */
+   // Lower-resolution images will be faster to process and result in less
+   // false-positives for contouring
+   cv::Mat img_lowres;
+   cv::resize(img_gray, img_lowres, cv::Size(160,128));
+   // Apply a Gaussian blur to reduce noise and therefore odds of false-positives
+   cv::Mat blurred;
+   cv::GaussianBlur(img_gray, blurred, cv::Size(5,5), 1.0);
 
-   //cv::Mat k_horz = (cv::Mat_<float>(3,3) <<
-   //      0, 0, 0,
-   //      1, 1, 1,
-   //      0, 0, 0 );
-   //cv::Mat img_filtered_horz;
-   //cv::filter2D(img_gray, img_filtered_horz, -1, k_horz);
+   /* Edge Detector */
+   cv::Mat edges;
+   cv::Canny(blurred, edges, 20, 50);
 
-   //// Let's try some Gaussian blurs
-   //double sigma = 10.0;
-   //// First, the built-in opencv function cv::GaussianBlur()
-   //cv::Mat img_gaussianblur_builtin;
-   //cv::GaussianBlur(img, img_gaussianblur_builtin, cv::Size(15,15), sigma);
-   //// Now, try first building the kernel using the other built-in kernel generating fcns
-   //cv::Mat k1d = cv::getGaussianKernel(15, sigma, CV_64F);
-   //cv::Mat k2d = k1d * k1d.t();
-   //cv::Mat img_gaussianblur_manualk;
-   //cv::filter2D(img, img_gaussianblur_manualk, -1, k2d);
+   /* Up-Scale Image */
+   cv::Mat edges_upscaled;
+   cv::resize(edges, edges_upscaled, og_size);
 
-   // Let's now try some edge detectors, starting with Sobel
-   // We've already got the grayscaled image, which is a typical first-step.
-   // Now, we'll blur it, and then apply the 1D gradient operations, then
-   // get the magnitude.
-   //cv::Mat img_gray_blurred, edges, binary;
-   //// Now that we've got the magnitude of the gradient, let's normalize and
-   //// threshold it before we display the image.
-   //cv::Mat img_edges_norm, img_edges_bin;
-   //cv::normalize(mag_grad, img_edges_norm, 0, 255, cv::NORM_MINMAX);
-   //cv::threshold(img_edges_norm, img_edges_bin, 50, 255, cv::THRESH_BINARY);
-
-   //cv::Mat img_edges_sobel = img_edges_bin;
-   //img_edges_sobel.convertTo(img_edges_sobel, CV_8U);
-   //cv::Mat img_edges_sobel_prethresh = img_edges_norm;
-   //img_edges_sobel_prethresh.convertTo(img_edges_sobel_prethresh, CV_8U);
-
-   // Let's try Canny out
-   cv::Mat img_gray_blurred, canny_edges, canny_binary;
-   cv::GaussianBlur(img_gray, img_gray_blurred, cv::Size(5,5), 1.0);
-   cv::Canny(img_gray_blurred, canny_edges, 20, 50);
-
+   /* Contour Detection */
    // Alrighty! Now let's have OpenCV find us the list of contours it can gather.
-   cv::Mat edges = canny_edges;
    std::vector<std::vector<cv::Point>> contours;
-   cv::findContours(edges, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+   cv::findContours(edges_upscaled, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
    // Filter smaller areas out
    std::vector<std::vector<cv::Point>> contours_filtered;
    for ( auto& contour : contours )
@@ -78,6 +50,7 @@ int main(void)
       if ( cv::contourArea(contour) >= 0.25 )
          contours_filtered.push_back(contour);
    }
+
    // std::vector<std::vector<cv::Point>> contours_filtered = {
    //    contours |
    //    std::views::filter(
@@ -86,7 +59,7 @@ int main(void)
    //           } )
    // };
 
-   // Let's draw the contours found on the original image
+   /* Draw Contours */
    const cv::Scalar contour_color(240, 32, 160); // appprox teal
    cv::Mat img_contourd = img.clone();
    cv::Mat img_contourd_filtered = img.clone();
@@ -104,8 +77,8 @@ int main(void)
    //cv::imshow("Gaussian Blurred Image (Manual Kernel)", img_gaussianblur_manualk);
    //cv::imshow("Sobel Pre-Threshold",                    img_edges_sobel_prethresh);
    //cv::imshow("Sobel'd Image",                          img_edges_sobel);
-   cv::imshow("Gray Blurred",                           img_gray_blurred);
-   cv::imshow("Canny Edges",                            canny_edges);
+   cv::imshow("Gray Blurred",                           blurred);
+   cv::imshow("Canny Edges",                            edges);
    cv::imshow("Contour'd Image",                        img_contourd);
    cv::imshow("Contour'd Image (Filtered)",             img_contourd_filtered);
 
